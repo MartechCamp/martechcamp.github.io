db7c1462e07f829f7842f7d3c3f073f16f5d2874
knowbots <59176261+knowbots@users.noreply.github.com>
2020-12-04 11:13:30 -0500

    heading 2 update on index

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const input = document.querySelector(".char--input");
+const log = document.querySelector(".char--count");
+
+input.addEventListener("input", function (e) {
+  console.log(e.target.value.length);
+  log.textContent = e.target.value.length;
+});
