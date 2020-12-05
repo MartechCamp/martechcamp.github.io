194adeb8854acbaeb98fa80360a3b73ca8f6bd8c
knowbots <59176261+knowbots@users.noreply.github.com>
2020-12-04 20:58:05 -0500

    Style update

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ input.addEventListener("input", function (e) {
   console.log(e.target.value.length);
   log.textContent = e.target.value.length;
 });
+
+if (a == b) {
+  console.log("they match!");
+}
+
+const newFunction = function (a, b) {
+  return a + b;
+};
